bd071657e6bcbecf1218901eb8a90b4997de10ec
romeu <romeuborges19@gmail.com>
2022-07-22 16:14:58 -0300

    Login page

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from recipes.views import home, my_view
+from recipes.views import home, my_view, register_page, tela_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('login/', tela_login),
+    path('register/', register_page)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 def my_view(request):
     return HttpResponse("TESTE 1")
+
+def tela_login(request):
+    return HttpResponse("TELA DE LOGIN")
+
+def register_page(request):
+    return HttpResponse("TELA DE CADASTRO")
